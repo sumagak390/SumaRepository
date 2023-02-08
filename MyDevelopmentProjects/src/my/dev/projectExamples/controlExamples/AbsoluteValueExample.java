a7c4661debc7c48cd89a4ac26075a76b94f8ce31
sumagak390 <124598737+sumagak390@users.noreply.github.com>
2023-02-08 14:54:34 -0500

    AbsoluteValueExample
    AbsoluteValueExample

---
+++ MyDevelopmentProjects/src/my/dev/projectExamples/controlExamples/AbsoluteValueExample.java
@@ -0,0 +1,17 @@
+package my.dev.projectExamples.controlExamples;
+
+import java.util.Scanner;
+
+public class AbsoluteValueExample {
+	public static void main(String[] args) {
+	System.out.println("Enter Absolute Value:");	
+	
+	Scanner scn = new Scanner(System.in);
+	int i = scn.nextInt();
+	System.out.println(Math.abs(i));
+	
+	
+
+}
+}
+
