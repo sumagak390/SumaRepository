3198369be414093cc39c668ef4f0e8811f4e2eb0
sumagak390 <124598737+sumagak390@users.noreply.github.com>
2023-02-08 14:57:18 -0500

    DiscountExample
    DiscountExample

---
+++ MyDevelopmentProjects/src/my/dev/projectExamples/controlExamples/DiscountExample.java
@@ -0,0 +1,34 @@
+package my.dev.projectExamples.controlExamples;
+
+import java.util.Scanner;
+
+public class DiscountExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Total cost of the purchased quantity");
+
+		int totalCost = scn.nextInt();
+
+		int discountPercent = 10;
+
+		if(totalCost>1000)
+		{
+			System.out.println("Total cost of money...." + totalCost);
+
+			int discountAmount=(totalCost*discountPercent)/100;
+
+			System.out.println("Discount money...." + discountAmount);
+
+			int totalBill = totalCost - discountAmount;
+
+			System.out.println("Total bill after discount...." + totalBill);
+
+		}
+		else 
+
+			System.out.println("No discount appiled ,your total bill is..."+ totalCost);
+	}
+
+}
